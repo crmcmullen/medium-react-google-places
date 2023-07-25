83a8173e8c4140ca9f557d0a6fd1d85819d22706
JCLimpide <54242762+JCLimpide@users.noreply.github.com>
2023-07-25 20:14:47 +0200

    [FEAT] Home design: separate form/result into MUI stepper + add papers

--- src/component/home.js
+++ src/component/home.js
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import {
+  Typography,
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+} from '@mui/material';
+
 import "./home.scss";
 
 import Form from './Form';
@@ -9,16 +18,48 @@ import { _MAP_CENTER_FR_ } from './config';
 function Home() {
   const [ center, setCenter ] = useState(_MAP_CENTER_FR_);
   const [ surface, setSurface ] = useState(0);
+  const [ activeStep, setActiveStep ] = useState(0);
+
+  const submitAddress = location => {
+    setCenter(location);
+    setActiveStep(1);
+  }
+
 
   return (
-    <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'stretch' }}>
-      <div>
-        <Form centerMap={setCenter} />
-        <div style={{ background: '#f2f2f2', padding: '1.5em', margin: '1em 0'}}>
-          <h4>Surface : { surface ? surface.toLocaleString('fr-FR', { maximumFractionDigits: 2 }) : '--'} m2</h4>
-        </div>
+    <div className="container">
+      <Typography variant='h5' color='primary' gutterBottom>
+        Calculer rendement solaire
+      </Typography>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'stretch' }}>
+        <Paper style={{ width: '100%', padding: '1rem' }}>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            <Step>
+              <StepLabel onClick={() => setActiveStep(0)}>Renseigner l'adresse</StepLabel>
+              <StepContent>
+                <Form centerMap={submitAddress} />        
+              </StepContent>
+            </Step>
+            <Step>
+              <StepLabel onClick={() => setActiveStep(1)}>Calculer les pans</StepLabel>
+              <StepContent>
+                <div style={{ background: '#f2f2f2', padding: '1.5em', margin: '1em 0'}}>
+                  <h4>Surface : { surface ? surface.toLocaleString('fr-FR', { maximumFractionDigits: 2 }) : '--'} m2</h4>
+                </div>
+              </StepContent>
+            </Step>
+            <Step onClick={() => setActiveStep(2)}>
+              <StepLabel>Enlever les obstacles</StepLabel>
+              <StepContent>
+                A DEFINIR
+              </StepContent>
+            </Step>
+          </Stepper>
+        </Paper>
+        <Paper>
+          <Map center={center} setSurface={setSurface} />
+        </Paper>
       </div>
-      <Map center={center} setSurface={setSurface} />
     </div>
   );
 }
